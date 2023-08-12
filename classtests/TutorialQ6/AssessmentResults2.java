b13bd76f43e3737055247db58affc1c95950f000
‘M1gael’ <smit.migael.johannes@gmail.com>
2023-08-12 03:02:40 +0200

    Finished all

--- classtests/TutorialQ6/AssessmentResults2.java
+++ classtests/TutorialQ6/AssessmentResults2.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class AssessmentResults2{
 
     public static boolean validateMarks(double mark , int total){
-        if ((0 < mark <= total) && (0 < total <= 100)) {
+        if (0 < mark && mark <= total) {
             return true;
         }
-        else{
+        else {
             return false;
         }
     }
 
     public static int calcPercentage(double mark , int total){
-        return (mark/total)*100;
+        return (int) ((mark / total) * 100);
     }
 
     public static String determineResults(int percentage){
@@ -30,23 +30,47 @@ public class AssessmentResults2{
         System.out.println("The student has obtained a : " + result);
     }
 
-    public static void main(String[] args){
-        Scanner kb = new Scanner(System.in);
-
-        System.out.print("Enter the test mark: ");
-        double testMark = kb.nextDouble(); nextLine(); // clear buffer
-
-        System.out.print("Enter the total of the test : ");
-        int testTotal = kb.nextDouble(); nextLine(); // clear buffer
-        
-        boolean bValidResult = validateMarks(testMark , testTotal);
-
-        if (bValidResult == true){
-            int percentage = calcPercentage(testMark , testTotal);
-            String result = determineResults(percentage);
-            displayValues(percentage , result);
-        }
+    public static void displaySummary(int pass , int fail , int total){
+        System.out.print("There are " + total + " student results for this module");
+        System.out.print("There are " + pass + " students who passed this module");
+        System.out.print("There are " + fail + " students who failed this module");
     }
 
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        boolean bAgain = true , bValidResult = false;
+        int percentage = 0 , studCount = 0 , studPass = 0 , studFail = 0 ;
+        final int testTotal = 80;
+        double testMark = 0;
+        String result = " ";
+
+        while (bAgain == true){
+            studCount++;
+
+            System.out.print("Enter the test mark: ");
+            testMark = kb.nextDouble(); kb.nextLine(); // clear buffer
+            
+
+            if (testMark == -1) {
+                displaySummary(studPass , studFail , studCount);
+                System.exit(0);
+            }
+            
+            bValidResult = validateMarks(testMark , testTotal);
+
+            if (bValidResult == true){
+                percentage = calcPercentage(testMark , testTotal);
+                result = determineResults(percentage);
+                if (result.equals("Pass")) {
+                    studPass++;
+                }
+                else{
+                    studFail++;
+                }
+
+                displayValues(percentage , result);
+            }
+       }
+    }
 }   
 
