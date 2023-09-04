08d7863f44d50efe0177c9c259c9e54c38190e27
‘M1gael’ <smit.migael.johannes@gmail.com>
2023-09-04 18:46:38 +0200

    replace ,escape sequences , string comparason updt

---
+++ Replace.java
@@ -0,0 +1,21 @@
+public class Replace {
+    
+    public static void main(String[] args){
+        String sentence = "i study at TUT";
+        String newSentence ;
+
+        newSentence = sentence.replace("TUT", "UP");
+    /*
+        ==Alternative==
+        newSentence.replace('a', '@')
+
+
+        ==Also==
+        #Use the same string for efficency
+        Sentence = Sentence.replace("TUT", "UP");
+
+     */
+    
+    
+    }
+}
