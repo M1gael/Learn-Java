6bcb361a7b897422886eaeea9693e4e2db892ce9
‘M1gael’ <smit.migael.johannes@gmail.com>
2023-09-04 18:46:59 +0200

    string compare update , replace ,escape sequence

---
+++ EscapeSequences.java
@@ -0,0 +1,24 @@
+public class EscapeSequences {
+    public static void main(String[] args) {
+        // \t - Inserts a tab
+        System.out.println("Hello\tWorld");
+        
+        // \n - Inserts a newline
+        System.out.println("Hello\nWorld");
+        
+        // \r - Carriage return
+        System.out.println("Hello\rWorld");
+        
+        // \f - Form feed
+        System.out.println("Hello\fWorld");
+        
+        // \' - Inserts a single quote
+        System.out.println("Hello\'World");
+        
+        // \" - Inserts a double quote
+        System.out.println("Hello\"World");
+        
+        // \\ - Inserts a backslash
+        System.out.println("Hello\\World");
+    }
+}
