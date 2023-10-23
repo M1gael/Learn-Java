39eece965aa5b055e58370e59546a75d9e0c809c
‘M1gael’ <smit.migael.johannes@gmail.com>
2023-10-24 00:56:40 +0200

    OOP

---
+++ ObjectsClasses/CreatingClasses/Car.java
@@ -0,0 +1,19 @@
+package ObjectsClasses.CreatingClasses;
+
+public class Car {
+    //Class members go here
+    
+    //Instance Variables: Define the attributes of objects.
+    String make;
+    String model;
+    int year;
+
+    //Instance Methods: Define actions or behaviors of objects.
+    void start(){
+        //implementation
+    }
+    
+    void drive(){
+        //implementation
+    }
+}
