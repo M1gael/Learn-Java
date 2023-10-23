f3b5c9ad5d5e5ba1b725694d93b775f905d34cd4
‘M1gael’ <smit.migael.johannes@gmail.com>
2023-10-24 01:18:00 +0200

    Added tips for classes

--- ObjectsClasses/Car.java
+++ ObjectsClasses/Car.java
@@ -44,3 +44,37 @@ public class Car {
         myCar.drive();
     }
 }
+
+/*
+5. Access Modifiers:
+
+    Definition: Access modifiers (public, private, protected) control the visibility of class members (variables and methods).
+
+6. Encapsulation:
+
+    Definition: Encapsulation is the bundling of data (properties) and methods that operate on the data into a single unit (class).
+
+7. Inheritance:
+
+    Definition: Inheritance allows a class to inherit properties and behaviors from another class.
+
+8. Polymorphism:
+
+    Definition: Polymorphism allows objects to be treated as instances of their parent class.
+
+9. Static Members:
+
+    Definition: static members belong to the class rather than instances.
+
+10. Class Data Members:
+
+    Definition: Class data members are shared among all instances of the class.
+
+11. Final Keyword:
+
+    Definition: final keyword can be used to make a class, method, or variable unmodifiable.
+
+12. Abstract Classes and Interfaces:
+
+    Definition: Abstract classes and interfaces provide a way to achieve abstraction in Java.
+ */
