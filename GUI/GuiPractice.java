8ac8149d06809821a4f9b50d13108e13cce47848
Migael <smit.migael.johannes@gmail.com>
2024-07-17 13:04:07 +0200

    Sync

--- GUI/GuiPractice.java
+++ GUI/GuiPractice.java
@@ -1,3 +1,4 @@
+package GUI;
 import javax.swing.*;
 public class GuiPractice {
     public static void main(String[] args){
---
+++ GUI/SwingBasic1.java
@@ -0,0 +1,12 @@
+package GUI;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class SwingBasic1{
+
+    public static void main(String[] args) {
+        
+
+    }
+}
