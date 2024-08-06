cac766eb1204977b6bee750ee273629a09ea6f49
M1gael <smit.migael.johannes@gmail.com>
2024-08-06 16:17:42 -0700

    Added potential faster version of prime check in a comment

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -36,7 +36,40 @@ public static void main (String[] args)
        System.out.println("Prime numbers from 1 to "+ iNumMax +" are :");
        System.out.println(primeNumbers);
 
+/* ==POTENTIALLY FASTER==
+import java.util.Scanner;
 
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a positive integer: ");
+        int limit = scanner.nextInt();
+        
+        System.out.println("Prime numbers up to " + limit + " are:");
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        
+        scanner.close();
+    }
+    
+    private static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+*/
 
 
 
